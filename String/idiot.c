8098a05bbbb214a4eb060e20b8495862f841dc87
FaizJAVA <95473946+FaizJAVA@users.noreply.github.com>
2021-12-04 21:43:38 +0530

    Add files via upload

---
+++ String/idiot.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+ void main()
+{
+ char a[30]="i am an idiot";
+  char b[30]="shajdas";
+  
+  strcpy(b,a);
+  printf("%s\n",b);
+ 
+ 
+}
---
+++ String/pract.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int length(char[]);
+int length(char s[]) {
+   int c = 0,i=0;
+   
+   for(i=0;s[i]!='\0';i++);
+     
+   return i;
+}
+
+int main()
+{
+    char s[100];
+    int x; 
+    printf("Enter a string\n");
+    gets(s);
+    x=length(s);
+    printf("%d",x);
+    return 0;
+}
